e91cfeae1b71a038040226443711b6cf32105ebc
D. Adamson <0xwebmilk@gmail.com>
2024-09-18 19:36:20 +0300

    Some changes

---
+++ app/core/operations/process/process_comments.py
@@ -0,0 +1,31 @@
+import datetime
+import spacy
+import re
+
+nlp = spacy.load('ru_core_news_sm')
+
+async def process_comments(text, date):
+    now = datetime.datetime.now()
+
+    if f"{now.strftime("%Y")}-{now.strftime("%m")}" != date[:7]:
+        return 0.0
+    else:
+        text = re.sub(r'[^\w\s.,]', '', text)
+
+        time_patterns = re.findall(r'(\d+(?:[.,]\d*)?)\s*(час[а-я]*|ч|minут[а-я]*|мин)', text)
+
+        hours = 0.0
+        minutes = 0.0
+
+        for amount, unit in time_patterns:
+            try:
+                amount = float(amount.replace(',', '.'))
+                if re.search(r'час[а-я]*|ч', unit):
+                    hours += amount
+                elif re.search(r'минут[а-я]*|мин', unit):
+                    minutes += amount
+            except:
+                pass
+
+        total_hours = hours + (minutes / 60.0)
+        return total_hours
---
+++ app/core/operations/process/save_tasks.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import json
+import os
+
+async def save_tasks(tasks: list, saves_path: str, saves_encoding: str) -> None:
+    date = datetime.now()
+    month = date.strftime('%B')
+
+    if os.path.exists(saves_path):
+        with open(saves_path, 'r', encoding=saves_encoding) as file:
+            existing_data = json.load(file)
+    else:
+        existing_data = {}
+
+    if month not in existing_data:
+        existing_data[month] = []
+
+    task_dict = {task['title']: task for task in existing_data[month]}
+
+    for task in tasks:
+        if task.get('status') != '5' and task.get('priority') != '1':
+            if task['title'] in task_dict:
+                task_dict[task['title']]['lead-time'] += 24
+                task_dict[task['title']]['touch-time'] = task['hour']
+            else:
+                task_dict[task['title']] = {'title': task.get('title'), 'lead-time': 24, 'touch-time': task['hour']}
+
+    existing_data[month] = list(task_dict.values())
+
+    with open(saves_path, 'w', encoding=saves_encoding) as file:
+        json.dump(existing_data, file, ensure_ascii=False, indent=4)
+
+    with open(saves_path, 'w', encoding=saves_encoding) as file:
+        json.dump(existing_data, file, ensure_ascii=False, indent=4)
