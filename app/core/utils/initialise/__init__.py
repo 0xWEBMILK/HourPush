167db58cc2cca87d30ffcb06963b6acfec40e755
D. Adamson <0xwebmilk@gmail.com>
2024-09-18 19:36:23 +0300

    Some changes

---
+++ app/core/utils/initialise/__init__.py
@@ -0,0 +1,3 @@
+from .bitrix_variables_initialise import bitrix_variables_initialise
+from .saves_variables_initialise import saves_variables_initialise
+from .database_variables_initialise import database_variables_initialise
