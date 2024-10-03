df8d830682b64f9ad1ff03f749073b22f95e82ef
D. Adamson <0xwebmilk@gmail.com>
2024-10-03 10:47:33 +0300

    Some changes

--- app/core/core.py
+++ app/core/core.py
@@ -4,7 +4,7 @@ import structlog
 from structlog.typing import FilteringBoundLogger
 
 from .models import BitrixModel, DatabaseModel
-from .operations.get import get_tasks, get_comments
+from .operations.get import get_tasks, get_comments, get_sprint, get_stages
 from .operations.process import save_tasks
 from .operations.database import save_to_database
 
@@ -19,14 +19,18 @@ async def run(*args, **kwargs):
     saves_config = await saves_variables_initialise()
     logger.info("Variables initialise | success")
 
+
     logger.info("Models initialise | started")
     bitrix_model = BitrixModel(
         token=bitrix_config.get('token'),
         host=bitrix_config.get('host'),
 
         get_comments_event=bitrix_config.get('get_comments_event'),
-        get_tasks_event=bitrix_config.get('get_tasks_event')
+        get_tasks_event=bitrix_config.get('get_tasks_event'),
+        get_sprint_event=bitrix_config.get('get_sprint_event'),
+        get_stages_event=bitrix_config.get('get_stages_event'),
     )
+
 
     database_model = DatabaseModel(
         sqlalchemy_database_uri = database_config.get('sqlalchemy_database_uri'),
@@ -34,19 +38,27 @@ async def run(*args, **kwargs):
     )
     logger.info("Models initialise | success")
 
+
+    bitrix_sprint_id = await get_sprint(bitrix_model)
+    bitrix_stages_ids = await get_stages(bitrix_model, bitrix_sprint_id)
+
+
     logger.info("Getting all tasks | started")
-    bitrix_tasks = await get_tasks(bitrix_model)
+    bitrix_tasks = await get_tasks(bitrix_model, bitrix_stages_ids)
     logger.info("Getting all tasks | success")
+
 
     logger.info("Calculating hours from comments | started")
     bitrix_tasks = await get_comments(bitrix_model, bitrix_tasks)
     logger.info("Calculating hours from comments | success")
+
 
     logger.info("Saving tasks | started")
     await save_tasks(bitrix_tasks,
                      saves_path=saves_config.get('saves_path'),
                      saves_encoding=saves_config.get('saves_encoding'))
     logger.info("Saving tasks | success")
+
 
     logger.info("Writing to database | started")
     await save_to_database(database_model,
