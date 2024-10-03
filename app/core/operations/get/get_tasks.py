acdaccab8de5a0cecfe21972a1e46539b10a71aa
D. Adamson <0xwebmilk@gmail.com>
2024-10-03 10:47:45 +0300

    Some changes

--- app/core/operations/get/get_tasks.py
+++ app/core/operations/get/get_tasks.py
@@ -1,14 +1,12 @@
-async def get_tasks(bitrix_model):
-    list_of_tasks, start = [], 0
+async def get_tasks(bitrix_model, bitrix_stage_ids):
+    list_of_tasks = []
 
-    while True:
-        response = await bitrix_model.get_tasks(start=start)
+    for bitrix_id in bitrix_stage_ids:
+        response = await bitrix_model.get_tasks(bitrix_id=bitrix_id["id"])
 
         tasks = response.get('result', {}).get('tasks', [])
-        if not tasks:
-            break
+        print(tasks)
 
         list_of_tasks.extend({**task, 'hour': 0.0} for task in tasks)
-        start += 50
 
     return list_of_tasks
