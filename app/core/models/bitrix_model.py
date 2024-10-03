9eb82082c0426076df1dd02aae893080810ba0b4
D. Adamson <0xwebmilk@gmail.com>
2024-10-03 10:47:36 +0300

    Some changes

--- app/core/models/bitrix_model.py
+++ app/core/models/bitrix_model.py
@@ -7,22 +7,53 @@ logger: FilteringBoundLogger = structlog.get_logger()
 
 
 class BitrixModel:
-    def __init__(self, host: str, token: str, get_comments_event: str, get_tasks_event: str, *args) -> None:
+    def __init__(self,
+                 host: str,
+                 token: str,
+                 get_comments_event: str,
+                 get_tasks_event: str,
+                 get_stages_event: str,
+                 get_sprint_event: str, *args) -> None:
+        
         self.host = host
         self.token = token
         self.base_url = f'{host}/rest/72'
         self.get_comments_event = get_comments_event
         self.get_tasks_event = get_tasks_event
+        self.get_stages_event = get_stages_event
+        self.get_sprint_event = get_sprint_event
+
 
     async def _action(self, event: str, **params):
         event_url = event
         if params:
-            event_url += '?' + '&'.join(f'{key}={value}' for key, value in params.items())
-        
+            query_params = []
+            for key, value in params.items():
+                if isinstance(value, dict):
+                    # Обрабатываем вложенные словари
+                    for sub_key, sub_value in value.items():
+                        query_params.append(f'{key}[{sub_key}]={sub_value}')
+                else:
+                    # Обрабатываем простые параметры
+                    query_params.append(f'{key}={value}')
+            
+            event_url += '?' + '&'.join(query_params)
+
         return await action(self.base_url, self.token, event_url)
+
+    
 
     async def get_comments(self, task_id: int):
         return await self._action(self.get_comments_event, id=task_id)
 
-    async def get_tasks(self, start: int = 0):
-        return await self._action(self.get_tasks_event, start=start, STATUS='2')
+
+    async def get_tasks(self, bitrix_id):
+        return await self._action(self.get_tasks_event, filter={"STAGE_ID": bitrix_id})
+
+    
+    async def get_sprint(self):
+        return await self._action(self.get_sprint_event)
+
+
+    async def get_stages(self, sprint_id):
+        return await self._action(self.get_stages_event, sprintId=sprint_id)
