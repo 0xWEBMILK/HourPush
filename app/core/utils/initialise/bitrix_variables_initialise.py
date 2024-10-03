b317984ea19db111e693482d6e55b7bfdb4322af
D. Adamson <0xwebmilk@gmail.com>
2024-10-03 10:47:52 +0300

    Some changes

--- app/core/utils/initialise/bitrix_variables_initialise.py
+++ app/core/utils/initialise/bitrix_variables_initialise.py
@@ -8,4 +8,6 @@ async def bitrix_variables_initialise() -> dict:
 
             "get_comments_event": main_settings.bitrix.get_comments_event.get_secret_value(),
             "get_tasks_event": main_settings.bitrix.get_tasks_event.get_secret_value(),
+            "get_stages_event": main_settings.bitrix.get_stages_event.get_secret_value(),
+            "get_sprint_event": main_settings.bitrix.get_sprint_event.get_secret_value(),
         }
