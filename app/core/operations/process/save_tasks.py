537f5005596adc2d73c8f64944ac0386e8b6d71d
D. Adamson <0xwebmilk@gmail.com>
2024-10-04 13:13:24 +0300

    Some changes

--- app/core/operations/process/save_tasks.py
+++ app/core/operations/process/save_tasks.py
@@ -17,8 +17,6 @@ async def save_tasks(tasks: list, saves_path: str, saves_encoding: str) -> None:
 
     task_dict = {task['title']: task for task in existing_data[month]}
 
-    # Status: 6 _\|/_ Отложена
-
     for task in tasks:
         if (task.get('status') != '5' and task.get('priority') != '1') or (task.get('status') == '6'):
             if task['title'] in task_dict:
@@ -28,9 +26,6 @@ async def save_tasks(tasks: list, saves_path: str, saves_encoding: str) -> None:
                 task_dict[task['title']] = {'title': task.get('title'), 'lead-time': 24, 'touch-time': task['hour']}
 
     existing_data[month] = list(task_dict.values())
-
-    with open(saves_path, 'w', encoding=saves_encoding) as file:
-        json.dump(existing_data, file, ensure_ascii=False, indent=4)
 
     with open(saves_path, 'w', encoding=saves_encoding) as file:
         json.dump(existing_data, file, ensure_ascii=False, indent=4)
