372a9e3fae95c716f8489dda363343b382cf4cab
D. Adamson <0xwebmilk@gmail.com>
2024-10-04 13:13:31 +0300

    Some changes

--- app/core/utils/initialise/saves_variables_initialise.py
+++ app/core/utils/initialise/saves_variables_initialise.py
@@ -1,7 +1,8 @@
 from ..config_setup import main_settings
+from typing import Dict
 
 
-async def saves_variables_initialise() -> dict:
+async def saves_variables_initialise() -> Dict[str, str]:
     return {
             "saves_path": main_settings.saves.saves_path.get_secret_value(),
             "saves_encoding": main_settings.saves.saves_encoding.get_secret_value(),
