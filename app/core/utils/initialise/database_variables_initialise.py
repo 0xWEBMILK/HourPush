1c78c279f1d266735b97be881d4088eac72a714b
D. Adamson <0xwebmilk@gmail.com>
2024-10-04 13:13:28 +0300

    Some changes

--- app/core/utils/initialise/database_variables_initialise.py
+++ app/core/utils/initialise/database_variables_initialise.py
@@ -1,7 +1,8 @@
 from ..config_setup import main_settings
+from typing import Dict
 
 
-async def database_variables_initialise() -> dict:
+async def database_variables_initialise() -> Dict[str, str]:
     return {
             "sqlalchemy_database_uri": main_settings.database.sqlalchemy_database_uri.get_secret_value(),
             "sqlalchemy_track_modifications": main_settings.database.sqlalchemy_track_modifications,
